969255a6d40b88a96a36bb7ee6e4571ed9a09de6
Rodolfo Luiz Soares dos Santos <rlsantos@sefnet.rj>
2018-01-18 15:47:55 -0200

    primeira versao do projeto

---
+++ src/main/java/com/rest/api/ProjectRestApplication.java
@@ -0,0 +1,12 @@
+package com.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestApplication.class, args);
+	}
+}
