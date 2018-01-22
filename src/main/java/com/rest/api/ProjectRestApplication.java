27055d8cc4840ddee5bbccbf650b2e0d73882b6b
rlsantos <rlsantos@fazenda.rj.gov.br>
2018-01-22 19:37:08 -0200

    Setting the project

--- src/main/java/com/rest/api/ProjectRestApplication.java
+++ src/main/java/com/rest/api/ProjectRestApplication.java
@@ -1,13 +1,32 @@
 package com.rest.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectRestApplication {
 
+	@Value("$paginacao.qtd_por_pagina")
+	private int qtdPorPagina;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectRestApplication.class, args);
 		System.out.println("Hello World");
 	}
+	
+	@Bean
+	public CommandLineRunner  commandLineRunner() {
+		
+		return args -> {
+			
+			System.out.println(" ### Quantidade de elementos na pagina = "  + this.qtdPorPagina);
+		};
+		
+	}
+	
+	
 }
